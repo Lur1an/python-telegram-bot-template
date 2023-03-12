dcbdd88c68d84f175d08c55d9a2cfa3498f7c40a
lurian <lurian-code@protonmail.com>
2023-03-12 19:10:18 +0100

    Restructured

--- src/bot/common/context.py
+++ src/bot/common/context.py
@@ -15,7 +15,6 @@ class BotData:
 
 
 class ChatData:
-    p: str
     pass
 
 
@@ -23,7 +22,7 @@ ConversationState = TypeVar("ConversationState")
 
 
 class UserData:
-    _conversation_state = {}
+    _conversation_state: Dict[Type[ConversationState], ConversationState] = {}
 
     def get_conversation_state(self, cls: Type[ConversationState]) -> ConversationState:
         return self._conversation_state[cls]
@@ -31,8 +30,8 @@ class UserData:
     def initialize_conversation_state(self, cls: Type[ConversationState]):
         self._conversation_state[cls] = cls()
 
-    def clean_up_conversation_state(self, cls: Type[ConversationState]):
-        del self._conversation_state[cls]
+    def clean_up_conversation_state(self, conversation_type: Type[ConversationState]):
+        del self._conversation_state[conversation_type]
 
 
 class ApplicationContext(CallbackContext[ExtBot, UserData, ChatData, BotData]):
@@ -40,31 +39,35 @@ class ApplicationContext(CallbackContext[ExtBot, UserData, ChatData, BotData]):
     pass
 
 
-def init_stateful_conversation(f: Callable[[Update, ApplicationContext], Awaitable[Any]],
-                               conversation_state_type: Type[ConversationState]):
-    @wraps(f)
-    async def wrapped(update: Update, context: ApplicationContext):
-        context.user_data.initialize_conversation_state(conversation_state_type)
-        return await f(update, context)
+def init_stateful_conversation(conversation_state_type: Type[ConversationState]):
+    def inner_decorator(f: Callable[[Update, ApplicationContext], Awaitable[Any]]):
+        @wraps(f)
+        async def wrapped(update: Update, context: ApplicationContext):
+            context.user_data.initialize_conversation_state(conversation_state_type)
+            return await f(update, context)
 
-    return wrapped
+        return wrapped
+
+    return inner_decorator
 
 
-def cleanup_stateful_conversation(f: Callable[[Update, ApplicationContext], Awaitable[Any]],
-                                  conversation_state_type: Type[ConversationState]):
+def cleanup_stateful_conversation(conversation_state_type: Type[ConversationState]):
     """
     Cleans up the user_data dict field that was holding onto the stateful conversation object and returns ConversationHandler.END,
     exceptions are ignored to ensure that the conversation state is cleaned up and the handlers ends
     """
 
-    @wraps(f)
-    async def wrapped(update: Update, context: ApplicationContext):
-        try:
-            await f(update, context)
-        except Exception as e:
-            log.warning(f"Encountered exception during last step of conversation: {e}")
-        finally:
-            context.user_data.clean_up_conversation_state(conversation_state_type)
-            return ConversationHandler.END
+    def inner_decorator(f: Callable[[Update, ApplicationContext], Awaitable[Any]]):
+        @wraps(f)
+        async def wrapped(update: Update, context: ApplicationContext):
+            try:
+                await f(update, context)
+            except Exception as e:
+                log.warning(f"Encountered exception during last step of conversation: {e}")
+            finally:
+                context.user_data.clean_up_conversation_state(conversation_state_type)
+                return ConversationHandler.END
 
-    return wrapped
+        return wrapped
+
+    return inner_decorator
--- src/db/core.py
+++ src/db/core.py
@@ -5,7 +5,39 @@ from motor.motor_asyncio import AsyncIOMotorCollection
 from pymongo.results import InsertOneResult
 from src.db.encoders import jsonable_encoder
 
-from src.db.models import MongoEntity
+from bson import ObjectId
+from pydantic import BaseModel, Field
+
+
+class PyObjectId(ObjectId):
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, v):
+        if not ObjectId.is_valid(v):
+            raise ValueError("Invalid objectid")
+        return ObjectId(v)
+
+    @classmethod
+    def __modify_schema__(cls, field_schema):
+        field_schema.update(type="string")
+
+
+class MongoEntity(BaseModel):
+    mongo_id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
+
+    @property
+    def id(self) -> str:
+        return str(self.mongo_id)
+
+    class Config:
+        allow_population_by_field_name = True
+        json_encoders = {
+            ObjectId: str,
+        }
+
 
 Entity = TypeVar("Entity", bound=MongoEntity)
 
