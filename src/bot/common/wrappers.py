ac2a6c92840663347c50b23efb1c594e1e1098f8
lurian <lurian-code@protonmail.com>
2023-03-12 18:08:23 +0100

    Fixed decorators

--- src/bot/common/wrappers.py
+++ src/bot/common/wrappers.py
@@ -24,22 +24,36 @@ def admin_command(f: Callable[[Update, ApplicationContext], Awaitable[Any]], adm
 CallbackDataType = TypeVar("CallbackDataType")
 
 
-def arbitrary_callback_query_handler(
-        f: Callable[[Update, ApplicationContext, Generic[CallbackDataType]], Awaitable[Any]], answer_query_after: bool = True):
+def arbitrary_callback_query_handler(query_data_type: CallbackDataType, answer_query_after: bool = True):
+    def inner_decorator(
+            f: Callable[[Update, ApplicationContext, Generic[CallbackDataType]], Awaitable[Any]]
+    ) -> CallbackQueryHandler:
+        decorator = inject_callback_query(answer_query_after=answer_query_after)
+        wrapped = decorator(f)
+        handler = CallbackQueryHandler(pattern=query_data_type, callback=wrapped)
+        return handler
+
+    return inner_decorator
+
+
+def inject_callback_query(answer_query_after: bool = True):
     """
     Transforms this method into a CallbackQueryHandler for the used CallbackDataType, the callback_query.data is injected into the wrapped function
     Automatically answers the callback query when the handler is done.
     """
 
-    @wraps(f)
-    async def wrapped(update: Update, context: ApplicationContext):
-        converted_data = cast(CallbackDataType, update.callback_query.data)
-        result = await f(update, context, converted_data)
-        if answer_query_after:
-            await update.callback_query.answer()
-        return result
+    def inner_decorator(f: Callable[[Update, ApplicationContext, Generic[CallbackDataType]], Awaitable[Any]]):
+        @wraps(f)
+        async def wrapped(update: Update, context: ApplicationContext):
+            converted_data = cast(CallbackDataType, update.callback_query.data)
+            result = await f(update, context, converted_data)
+            if answer_query_after:
+                await update.callback_query.answer()
+            return result
 
-    return CallbackQueryHandler(pattern=CallbackDataType, callback=wrapped)
+        return wrapped
+
+    return inner_decorator
 
 
 def cleanup_chat(f: Callable[[Update, ApplicationContext, list[tuple[int, int]]], Awaitable[Any]]):
@@ -53,7 +67,7 @@ def cleanup_chat(f: Callable[[Update, ApplicationContext, list[tuple[int, int]]]
         delete_after: list[tuple[int, int]] = list()
         # call method
         result = await f(update, context, delete_after)
-        # clean up
+        # cleanup up
         for message_id, chat_id in delete_after:
             try:
                 await context.bot.delete_message(
@@ -71,17 +85,18 @@ def delete_message_after(f: Callable[[Update, ApplicationContext], Awaitable[Any
     @wraps(f)
     async def wrapper(update: Update, context: ApplicationContext):
         result = await f(update, context)
-        await context.bot.delete_message(
-            message_id=update.effective_message.id,
-            chat_id=update.effective_chat.id
-        )
-        return result
+        try:
+            await context.bot.delete_message(
+                message_id=update.effective_message.id,
+                chat_id=update.effective_chat.id
+            )
+        finally:
+            return result
 
     return wrapper
 
 
 def exit_conversation_on_exception(
-        f: Callable[[Update, ApplicationContext], Any],
         user_message: str = "I'm sorry, something went wrong, try again or contact an Administrator."
 ):
     """
@@ -91,16 +106,20 @@ def exit_conversation_on_exception(
     :return:
     """
 
-    @wraps(f)
-    async def wrapped(update: Update, context: ApplicationContext):
-        try:
-            return await f(update, context)
-        except:
-            await context.bot.send_message(
-                chat_id=update.effective_chat.id,
-                text=user_message
-            )
+    def inner_decorator(f: Callable[[Update, ApplicationContext], Any]):
+
+        @wraps(f)
+        async def wrapped(update: Update, context: ApplicationContext):
+            try:
+                return await f(update, context)
+            except:
+                await context.bot.send_message(
+                    chat_id=update.effective_chat.id,
+                    text=user_message
+                )
             context.chat_data.conversation_data = None
             return ConversationHandler.END
 
-    return wrapped
+        return wrapped
+
+    return inner_decorator
