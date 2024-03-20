90708c0ca6a7a98ea5702a76c5f462b3f459d25c
lur1an <lurian-code@protonmail.com>
2024-03-20 15:16:34 +0100

    fixed deprecated pydantic encoder in json serializer for SQLAlchemy

--- src/db/config.py
+++ src/db/config.py
@@ -1,12 +1,12 @@
 from sqlalchemy.ext.asyncio import create_async_engine
 import json
-import pydantic.json
+import pydantic_core
 
 def json_serializer(*args, **kwargs) -> str:
     """
     Encodes json in the same way that pydantic does.
     """
-    return json.dumps(*args, default=pydantic.json.pydantic_encoder, **kwargs)
+    return json.dumps(*args, default=pydantic_core.to_jsonable_python, **kwargs)
 
 def create_engine(db_path: str):
     db_url = "sqlite+aiosqlite:///" + db_path
